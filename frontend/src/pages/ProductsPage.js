9d32ed89cd39dc3332b358a38ead30b25566f953
Topmewan <anon8733@gmail.com>
2021-10-08 23:11:10 +0300

    add logick for slider

--- frontend/src/pages/ProductsPage.js
+++ frontend/src/pages/ProductsPage.js
@@ -1,5 +1,17 @@
-import {CircularProgress, Container, Grid, makeStyles, Slider, Typography, Paper, TextField} from "@material-ui/core";
+import {
+    CircularProgress,
+    Container,
+    Grid,
+    makeStyles,
+    Slider,
+    Typography,
+    Paper,
+    TextField,
+    Radio,
+    FormControl, RadioGroup, FormControlLabel
+} from "@material-ui/core";
 import {useState, useEffect} from "react";
+import {useHistory,useLocation} from "react-router-dom";
 
 import axios from "axios";
 
@@ -14,16 +26,37 @@ const useStyles = makeStyles({
         display:'flex',
         justifyContent:'center',
         alignItems: 'center',
+    },
+    paper: {
+       marginBottom: '1rem',
+        padding: '13px',
+    },
+    filters: {
+       padding: "0 1.5rem",
+    },
+    priceRangeInputs: {
+       display: 'flex',
+        justifyContent: 'space-between',
     }
 })
 
 
 const ProductsPage = () =>  {
-
+    //Material Ui style
     const classes = useStyles();
+    const history = useHistory();
+    const location = useLocation();
 
+    const params = location.search ? location.search : null;
+
+    // Component state
     const [products, setProducts] = useState([]);
     const [loading,setLoading] = useState(false);
+
+    const [sliderMax, setSliderMax] = useState(1000);
+    const[priceRange,setPriceRange] = useState([25,75]);
+
+    const[filter,setFilter] = useState('');
 
     useEffect(() => {
         let cancel;
@@ -31,9 +64,17 @@ const ProductsPage = () =>  {
         const fetchData = async () => {
             setLoading(true);
             try {
+                let query;
+
+                if(params && !filter) {
+                    query = params;
+                } else {
+                    query = filter;
+                }
+
                 const {data} = await axios({
                     method: 'GET',
-                    url: `/api/v1/tz`,
+                    url: `/api/v1/tz${query}`,
                     cancelToken: new axios.CancelToken((c) => (cancel = c))
                 });
 
@@ -47,22 +88,39 @@ const ProductsPage = () =>  {
 
 
     fetchData();
-    },[])
+
+    },[filter,params]);
+
+    const onSliderCommitHandler = (e,newValue) => {
+        buildRangeFilter(newValue);
+    };
+
+    const buildRangeFilter = (newValue) => {
+        const urlFilter =`?price[gte]=${newValue[0]}&price[lte]=${newValue[1]}`;
+
+        setFilter(urlFilter);
+
+        history.push(urlFilter);
+    }
 
     return (
         <Container className={classes.root}>
-            <Paper>
+            <Paper className={classes.paper}>
                 <Grid container>
                     <Grid item xs={12} sm={6}>
                         <Typography gutterBottom>Фильтр</Typography>
 
-                        <div>
+                        <div className={classes.filters}>
                             <Slider
                                 min={0}
-                                max={100}
+                                max={sliderMax}
+                                value={priceRange}
+                                valueLabelDisplay='auto'
+                                onChange={(e, newValue)=> setPriceRange(newValue)}
+                                onChangeCommitted={onSliderCommitHandler}
                             />
 
-                            <div>
+                            <div className={classes.priceRangeInputs}>
                                 <TextField
                                     size='small'
                                     id='lower'
@@ -84,6 +142,30 @@ const ProductsPage = () =>  {
                                 />
                             </div>
                         </div>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <Typography gutterBottom>Сортировать</Typography>
+
+                        <FormControl component='fieldset' className={classes.filters}>
+                            <RadioGroup
+                                aria-label='price-order'
+                                name='price-order'
+                            >
+                                <FormControlLabel
+                                    control={<Radio/>}
+                                    label='Цена: По убыванию'
+                                    disabled={loading}
+                                />
+
+                                <FormControlLabel
+                                    control={<Radio/>}
+                                    label='Цена: По возрастанию'
+                                    disabled={loading}
+                                />
+                            </RadioGroup>
+                        </FormControl>
+
                     </Grid>
                 </Grid>
             </Paper>
